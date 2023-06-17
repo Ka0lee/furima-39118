f89b4e8d93af4bce2c0545ed450fed5341bb37e0
Ka0lee <pekochan90@gmail.com>
2023-06-18 00:59:38 +0900

    emiailアドレス入力時の＠制約

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,20 +5,25 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
          
          validates :nickname,presence:true
-
-         with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } do
+        
+         #漢字制約
+        with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } do
           validates :first_name
           validates :last_name
         end
 
+        #カタカナ制約
         with_options presence: true, format: { with: /\A[ァ-ヶ]+\z/ } do
          validates :last_name_kana
          validates :first_name_kana
         end
 
          validates :birth_date,presence:true
-         
-         validates :email, uniqueness: true
+
+        #@制約
+        with_options puniqueness: true, format: { with: /\A[\w+-.]+@[a-z\d-]+\z/i } do
+         validates :email
+        end
 
 
 
