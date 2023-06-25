8a870b61cf779c668636887d6488894ac0024099
Ka0lee <pekochan90@gmail.com>
2023-06-25 20:48:25 +0900

    shipping_fee_status_id(配送料負担)メニューの作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,4 +6,6 @@ class Item < ApplicationRecord
   validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
   belongs_to :sales_status_id
   validates :sales_status_id, numericality: { other_than: 1 , message: "can't be blank"}
+  belongs_to :shipping_fee_status_id
+  validates :shipping_fee_status_id, numericality: { other_than: 1 , message: "can't be blank"}
 end
