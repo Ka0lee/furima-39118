a5e0cbb163ec0747edaacfc4ae4503e98bb34f8f
Ka0lee <pekochan90@gmail.com>
2023-06-25 20:24:01 +0900

    sales_status_idとのアソシエーション追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,4 +4,6 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category_id
   validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
+  belongs_to :sales_status_id
+  validates :sales_status_id, numericality: { other_than: 1 , message: "can't be blank"}
 end
