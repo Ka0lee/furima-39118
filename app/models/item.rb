20b2a3bf7cc291b56844d2b415e1e0b3adf01c7b
Ka0lee <pekochan90@gmail.com>
2023-06-25 21:09:39 +0900

    scheduled_delivery_idの作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,11 +4,19 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category_id
   validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
+
   belongs_to :sales_status_id
   validates :sales_status_id, numericality: { other_than: 1 , message: "can't be blank"}
+
   belongs_to :shipping_fee_status_id
   validates :shipping_fee_status_id, numericality: { other_than: 1 , message: "can't be blank"}
+
   belongs_to :prefeture_id
   validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
+
+  belongs_to :scheduled_derivery_id
+  validates :scheduled_derivery_id, numericality: { other_than: 1 , message: "can't be blank"}
+
+
 
 end
