7ca4489d9d173e7456c3a962595f0995bb870383
Ka0lee <pekochan90@gmail.com>
2023-06-25 21:00:50 +0900

    prefecture_id(発送元住所)メニューの作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,4 +8,7 @@ class Item < ApplicationRecord
   validates :sales_status_id, numericality: { other_than: 1 , message: "can't be blank"}
   belongs_to :shipping_fee_status_id
   validates :shipping_fee_status_id, numericality: { other_than: 1 , message: "can't be blank"}
+  belongs_to :prefeture_id
+  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
+
 end
