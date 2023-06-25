6249c3246aa44aa43f607108676d0b57b477af94
Ka0lee <pekochan90@gmail.com>
2023-06-25 20:19:49 +0900

    category_idのアソシエーション追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,7 @@
 class Item < ApplicationRecord
   belongs_to :user
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category_id
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
 end
