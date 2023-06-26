668bea818912d53a0b69d95f9473e8aedad3e583
Ka0lee <pekochan90@gmail.com>
2023-06-27 01:49:14 +0900

    ルーティングの修正（articles削除）

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,7 @@ Rails.application.routes.draw do
 
   devise_for :users
   root to: 'items#index'
-  resources :items, only:[:index, :new, :create,:show, :destroy]
-  resources :articles,only:[:index, :new, :create]
-
+  resources :items, only:[:index, :new, :create]
+  
 
 end
