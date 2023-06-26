0b5d82676cee9e7c6bf894a35cf8792885d18e97
Ka0lee <pekochan90@gmail.com>
2023-06-27 01:48:25 +0900

    newアクション定義とストロングパラメーター設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -12,8 +12,23 @@ class ItemsController < ApplicationController
   end
 
   def create
+    @item = Item.new(item_params)
+
+    if @item.save
+      redirect_to root_path 
+    else
+      render :new 
+    end
   end
 
   def destroy
   end
+
+  
+  private
+    def item_params
+      params.require(:item).permit(:image, :name, :price, :info, :category_id,:sales_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id,:user).merge(user_id: current_user.id)
+    end
+
+
 end
