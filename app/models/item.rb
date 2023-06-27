7b427b78b8df03c54bad911c4dda18637c8cb9d9
Ka0lee <pekochan90@gmail.com>
2023-06-27 23:09:14 +0900

    画像のバリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,32 +1,30 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
-  
+
   validates :name, presence: true
-  validates :price, presence: true
+
+  #with_options presence: true, format: { with: /\A[0-9]+\z/ } do
+   # validates :price, numericality: { only_integer: true,greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+  #end
   validates :info, presence: true
-  validates :category_id, presence: true
-  validates :sales_status_id, presence: true
-  validates :shipping_fee_status_id, presence: true
-  validates :prefecture_id, presence: true
-  validates :scheduled_delivery_id, presence: true
+  validates :image, presence: true
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
-  validates :category, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :category_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
 
   belongs_to :sales_status
-  validates :sales_status, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :sales_status_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
 
   belongs_to :shipping_fee_status
-  validates :shipping_fee_status, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :shipping_fee_status_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
 
   belongs_to :prefecture
-  validates :prefecture, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :prefecture_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
 
-  belongs_to :scheduled_derivery
-  validates :scheduled_derivery, numericality: { other_than: 1 , message: "can't be blank"}
-
+  belongs_to :scheduled_delivery
+  validates :scheduled_delivery_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
 
 
 end
