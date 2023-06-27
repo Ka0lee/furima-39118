4e8c0664ff1ec22574770928a4774e46a9e69b44
Ka0lee <pekochan90@gmail.com>
2023-06-27 23:11:22 +0900

    販売価格の整数制限、300～9999999円までの入力制限、半角数字のみ入力制限のバリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,9 +4,9 @@ class Item < ApplicationRecord
 
   validates :name, presence: true
 
-  #with_options presence: true, format: { with: /\A[0-9]+\z/ } do
-   # validates :price, numericality: { only_integer: true,greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
-  #end
+  with_options presence: true, format: { with: /\A[0-9]+\z/ } do
+    validates :price, numericality: { only_integer: true,greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+  end
   validates :info, presence: true
   validates :image, presence: true
 
