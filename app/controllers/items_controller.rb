48093bf1129fc075b6df9004223baaef06ac6a06
Ka0lee <pekochan90@gmail.com>
2023-06-27 23:11:46 +0900

    ストロングパラメーターの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,19 +1,20 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!,except:[:index,:show]
+  before_action :authenticate_user!,except:[:index]
 
   def index
+    @items = Item.all
   end
 
   def new
     @item = Item.new
+    
   end
 
-  def show
-  end
+  #def show
+  #end
 
   def create
     @item = Item.new(item_params)
-
     if @item.save
       redirect_to root_path 
     else
@@ -21,13 +22,13 @@ class ItemsController < ApplicationController
     end
   end
 
-  def destroy
-  end
+  #def destroy
+  #end
 
   
   private
     def item_params
-      params.require(:item).permit(:image, :name, :price, :info, :category_id,:sales_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id,:user).merge(user_id: current_user.id)
+      params.require(:item).permit(:image, :name, :price, :info, :category_id,:sales_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id).merge(user_id: current_user.id)
     end
 
 
