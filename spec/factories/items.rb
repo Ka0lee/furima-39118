f43e9a0aa650f22c36348bb32f8d60fed2cf7c3b
Ka0lee <pekochan90@gmail.com>
2023-06-30 18:44:45 +0900

    FactoryBotの設定

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,20 @@
 FactoryBot.define do
   factory :item do
-    
+    name {"てすと"}
+    price {"1000"}
+    info {Faker::Lorem.sentence}
+    category_id {2}
+    sales_status_id {2}
+    shipping_fee_status_id {2}
+    prefecture_id {2}
+    scheduled_delivery_id {2}
+
+    association :user 
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/inu.jpg'), filename: 'inu.jpg')
+    end
+
+
   end
 end
