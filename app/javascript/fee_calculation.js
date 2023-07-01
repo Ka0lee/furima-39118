24a37b7b9b39f91f18a5aa5ab45b24ee550a8716
Ka0lee <pekochan90@gmail.com>
2023-07-01 13:43:50 +0900

    Math.roundをMath.floorへ変更

--- app/javascript/fee_calculation.js
+++ app/javascript/fee_calculation.js
@@ -3,13 +3,13 @@ window.addEventListener('load', () => {
   if (!priceInput){ return false;}
    priceInput.addEventListener("input", () => {
     const inputValue = priceInput.value;
-    console.log(inputValue);
+    //console.log(inputValue);
     const addTaxDom = document.getElementById("add-tax-price");
-    addTaxDom.innerHTML = Math.round(inputValue * 0.1 );
-     console.log(addTaxDom.innerHTML);
+    addTaxDom.innerHTML = Math.floor(inputValue * 0.1 );
+     //console.log(addTaxDom.innerHTML);
     const addPofitDom = document.getElementById("profit");
-     addPofitDom.innerHTML = Math.round(inputValue - Math.round(inputValue * 0.1 ))
-     console.log(addPofitDom.innerHTML);
+     addPofitDom.innerHTML = Math.floor(inputValue - Math.floor(inputValue * 0.1 ))
+     //console.log(addPofitDom.innerHTML);
 
     })
 })
