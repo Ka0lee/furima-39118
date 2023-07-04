2338be6130db33a809a40f38edbf357aac9646cb
Ka0lee <pekochan90@gmail.com>
2023-07-04 23:31:41 +0900

    showアクション定義とbefore_action :authenticate_user!にshow追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!,except:[:index]
+  before_action :authenticate_user!,except:[:index,:show]
 
   def index
     @items = Item.all.order(created_at: :desc)
@@ -10,8 +10,9 @@ class ItemsController < ApplicationController
     
   end
 
-  #def show
-  #end
+  def show
+    @item = Item.find(params[:id]) 
+  end
 
   def create
     @item = Item.new(item_params)
