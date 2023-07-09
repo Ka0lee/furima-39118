ced8c82c83c65afb3227627cfa1784c90ffd146d
Ka0lee <pekochan90@gmail.com>
2023-07-10 00:12:27 +0900

    配送先のformオブジェクトの実装

---
+++ app/models/order_pay_form.rb
@@ -0,0 +1,21 @@
+class OrderPayForm
+
+  include ActiveModel::Model
+  attr_accessor :postal_code, :prefecture, :city, :address, :building, :phone_number, :user_id, :item_id, :order_id
+
+  with_options presence: true do
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :city
+    validates :address
+    validates :phone_number, format: {/\A\d{10,11}\z/}
+    validates :user_id
+  end
+  
+  validates :prefecture, numericality: {other_than: 0, message: "can't be blank"}
+
+  def save
+    order = Order.create(item_id: item_id, user_id: user_id)
+    PayForm.create(postal_code: postal_code, prefecture: prefecture, city: city, address: address, building: building, phone_number: phone_number, order_id: order.id)
+  end
+
+end
