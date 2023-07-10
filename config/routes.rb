cd03569dcd40a41bf7a86b3e934cca7e7183d319
Ka0lee <pekochan90@gmail.com>
2023-07-10 21:30:02 +0900

    orderのルーティングをネスト

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
-  resources :items, only:[:index, :new, :create, :show, :edit, :update, :destroy]
-  resources :orders,only:[:index, :new, :create]
-  resources :pay_form,only: :create
+  resources :items do
+   resources :orders,only:[:index, :new, :create]
+   #resources :pay_form,only: :create
+  end
 
 end
