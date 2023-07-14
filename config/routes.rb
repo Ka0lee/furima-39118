5d53a4ccc87b687ab050e0247b66fcd4280231c0
Ka0lee <pekochan90@gmail.com>
2023-07-14 14:52:22 +0900

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   root to: 'items#index'
   resources :items do
    resources :orders,only:[:index, :new, :create]
-   #resources :pay_form,only: :create
   end
 
 end
