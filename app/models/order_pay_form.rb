0cb7debac218be8f5c7592366b268bce7005a397
Ka0lee <pekochan90@gmail.com>
2023-07-14 21:57:24 +0900

    attr_accessorの記述修正

--- app/models/order_pay_form.rb
+++ app/models/order_pay_form.rb
@@ -1,14 +1,16 @@
 class OrderPayForm
 
+
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture, :city, :address, :building, :phone_number, :user_id, :item_id, :order_id
+  attr_accessor :postal_code, :prefecture, :city, :address, :building, :phone_number, :user_id, :item_id, :order_id, :token
 
   with_options presence: true do
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :city
     validates :address
-    validates :phone_number, format: {/\A\d{10,11}\z/}
-    validates :user_id
+    validates :phone_number, format: {with: /\A\d{10,11}\z/}
+    validates :token
+
   end
   
   validates :prefecture, numericality: {other_than: 0, message: "can't be blank"}
