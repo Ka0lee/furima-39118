fd377f66a6425e8c7eb741e2d1544451739612c9
Ka0lee <pekochan90@gmail.com>
2023-07-25 22:15:49 +0900

    エラーコードの日本語化

--- app/models/order_pay_form.rb
+++ app/models/order_pay_form.rb
@@ -5,7 +5,7 @@ class OrderPayForm
   attr_accessor :postal_code, :prefecture, :city, :address, :building, :phone_number, :user_id, :item_id, :order_id, :token
 
   with_options presence: true do
-    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/}
     validates :city
     validates :address
     validates :phone_number, format: {with: /\A\d{10,11}\z/}
@@ -15,7 +15,7 @@ class OrderPayForm
 
   end
   
-  validates :prefecture, numericality: {other_than: 1, message: "can't be blank"}
+  validates :prefecture, numericality: {other_than: 1}
 
   def save
     order = Order.create(item_id: item_id, user_id: user_id)
